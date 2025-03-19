61e0bb51356fe2313304568b1a5e8ce0abfd89d9
Alexsander <131073044+allekstat@users.noreply.github.com>
2025-03-18 22:04:20 -0300

    Add files via upload

---
+++ csharp/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿class Pessoa
+{
+    public string Nome;
+    public int Idade;
+    public string Cpf;
+    public string Rg;
+    public string Cnh;
+    public float Peso;
+    public float Altura;
+    public string Etnia;
+    public string Estado_civil;
+    public string Profissao;
+    public string Nacionalidade;
+
+    public float MetrosPorSegundo;
+    public float LitrosDeArNoPulmao;
+    public float IdadeDoArNoPulmão;
+    public void Andar() { MetrosPorSegundo = 1.2; }
+    public void Correr() { MetrosPorSegundo = 6.5; }
+    public void Parar() { MetrosPorSegundo = 0; }
+    public void Inspirar() { LitrosDeArNoPulmao += 0.1; }
+    public void Espirar() { MetrosPorSegundo = 0; }
+}
---
+++ csharp/Programa.cs
@@ -0,0 +1,10 @@
+﻿class Programa
+{
+    static void Main(string[] args)
+    {
+        Pessoa[] alunos = new Pessoa[40];
+        alunos[0] = new Pessoa();
+        alunos[0].nome = "Alexsander";
+        alunos[0].idade = 1;
+    }
+}
