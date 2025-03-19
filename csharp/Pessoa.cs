6c436b80245a7efaede77a7f6f74324af7abf674
Alexsander <131073044+allekstat@users.noreply.github.com>
2025-03-18 22:31:00 -0300

    Add files via upload

--- csharp/Pessoa.cs
+++ csharp/Pessoa.cs
@@ -5,19 +5,38 @@
     public string Cpf;
     public string Rg;
     public string Cnh;
-    public float Peso;
-    public float Altura;
+    public double Peso;
+    public double Altura;
     public string Etnia;
     public string Estado_civil;
     public string Profissao;
     public string Nacionalidade;
 
-    public float MetrosPorSegundo;
-    public float LitrosDeArNoPulmao;
-    public float IdadeDoArNoPulmão;
+    public double MetrosPorSegundo;
+    public double LitrosDeArNoPulmao;
+    public double IdadeDoArNoPulmao;
     public void Andar() { MetrosPorSegundo = 1.2; }
     public void Correr() { MetrosPorSegundo = 6.5; }
     public void Parar() { MetrosPorSegundo = 0; }
     public void Inspirar() { LitrosDeArNoPulmao += 0.1; }
-    public void Espirar() { MetrosPorSegundo = 0; }
+    public void Espirar() { LitrosDeArNoPulmao *= 0.9; }
+    public void Respirar()
+    {
+        if (LitrosDeArNoPulmao < 1.4 && IdadeDoArNoPulmao < 1.8)
+        {
+            Inspirar();
+            IdadeDoArNoPulmao += 0.2;
+        }
+        else if (LitrosDeArNoPulmao > 0.15)
+        {
+            Espirar();
+            IdadeDoArNoPulmao += 0.2;
+        }
+        else
+        {
+            IdadeDoArNoPulmao += 0.2;
+        }
+    }
+
+    public void Falar() { Espirar(); }
 }
