789982c18ff0c96a280f131e053d6b4ea9722979
Alexsander <131073044+allekstat@users.noreply.github.com>
2025-04-14 22:26:57 -0300

    Add files via upload

--- php/calculo.php
+++ php/calculo.php
@@ -1,10 +1,24 @@
 <?php
-require "Pessoa.php";
 
-$nova_pessoa = new Pessoa();
-$nova_pessoa->nome = $_POST['nome'];
-$nova_pessoa->idade = $_POST['idade'];
-$nova_pessoa->peso = $_POST['peso'];
-$nova_pessoa->altura = $_POST['altura'];
+include 'cabecalho.php';
 
-echo $nova_pessoa->calcular_imc();
+require 'Pessoa.php';
+
+$pessoa = new Pessoa();
+$pessoa->nome = $_POST['nome'];
+$pessoa->idade = $_POST['idade'];
+$pessoa->peso = $_POST['peso'];
+$pessoa->altura = $_POST['altura'];
+$pessoa->calcular_imc();
+$pessoa->condicao_imc();
+
+$classificacao = $pessoa->classificar_imc();
+
+$icone = $pessoa->icone_imc();
+
+echo "<div class='border border-light-subtle rounded p-3 text-center container bg-$icone-subtle'>";
+
+echo "$pessoa->nome, com $pessoa->idade anos, pesando $pessoa->peso (kg) e com altura de $pessoa->altura (m) está na condição $classificacao com o imc valendo $pessoa->imc";
+
+echo '</div>';
+include 'rodape.php';
